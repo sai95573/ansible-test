d76eed5bec0b9496eb299f845c5b6faef66fc547
saikrishna <sai95573@gmail.com>
2023-04-28 01:44:28 +0530

    ansible test

--- mail.tf
+++ mail.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "mail_server" {
   count = var.mail_want == true ? var.mail_count : 0 
-  ami           = ami-0d0cd93f2bb1fdf17
+  ami           = "ami-0d0cd93f2bb1fdf17"
   instance_type = var.instance_type
   key_name      = var.key_name
   
--- web.tf
+++ web.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web_server" {
   count = var.web_want == true ? var.web_count : 0
-  ami           = ami-0d0cd93f2bb1fdf17
+  ami           = "ami-0d0cd93f2bb1fdf17"
   instance_type = var.instance_type
   key_name      = var.key_name
   
