e87a9a0927aca24cd5bfd3f249a2bb1992787248
saikrishna <sai95573@gmail.com>
2023-04-28 13:00:46 +0530

    ami updated

--- mail.tf
+++ mail.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "mail_server" {
   count = var.mail_want == true ? var.mail_count : 0 
-  ami           = "ami-0d0cd93f2bb1fdf17"
+  ami           = "ami-02eb7a4783e7e9317"
   instance_type = var.instance_type
   key_name      = var.key_name
   
